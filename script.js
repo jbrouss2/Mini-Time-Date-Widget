29aa22e30f6f771bd0db9b866fd416591038ea2a
Juliana Broussard <jbrouss2@vols.utk.edu>
2023-07-16 10:39:25 -0400

    Creating the 3 pages and doing basic work for each

---
+++ script.js
@@ -0,0 +1,27 @@
+let dateContainer = document.querySelector(".date-continer");
+let clockContainer = document.querySelector(".clocker-container");
+
+/*Array of all the week days*/
+const weekdays = [
+    "Sunday", 
+    "Monday", 
+    "Tuesday", 
+    "Wednesday", 
+    "Thursday", 
+    "Friday", 
+    "Saturday"
+];
+
+const monthNames = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "November",
+    "December"
+];
