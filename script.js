68c81aadbe08f68a1489cd3ad67281be84db80b0
Juliana Broussard <jbrouss2@vols.utk.edu>
2023-07-16 10:49:32 -0400

    Wrote JS for the clock functionality

--- script.js
+++ script.js
@@ -25,3 +25,17 @@ const monthNames = [
     "November",
     "December"
 ];
+
+const dateClock = setInterval(function dateTime(){
+    const today = new Date();
+    let date = today.getDate();
+    let day = weekdays[today.getDay()];
+    let month = monthNames[today.getMonth()];
+
+    let hours = today.getHours();
+    let minutes = today.getMinutes();
+
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+
+    dateContainer.innerHtml = `<p>${day}</p><p><span>${date}</span></p><p>${month}</p>`;
+})
